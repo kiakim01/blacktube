7d8207c626b293f64ca73e88e6f1f0b4c9c47682
chanho lee <leech3878@naver.com>
2023-09-05 20:11:10 +0900

    video 모델 프로토콜 채택 및 Detail 좋아요 구현

--- blacktube/Sources/ViewControllers/DetailViewController.swift
+++ blacktube/Sources/ViewControllers/DetailViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 import WebKit
 
+// 일반적인 배열 이용 : Video 구조체가 Equatable 프로토콜 채택해야함. 삭제로직 귀찮
+
+
+// Dictionary ["video id" : video]로 배열 선언 : tableView Cell 생성과정에서 indexPath를 NSIndexPath로 캐스팅 해주어야함.
+//var likedVideos: [String:Video] = [:]
+
 class DetailViewController: UIViewController {
     var video: Video?
     @IBOutlet weak var videoView: WKWebView!
@@ -16,6 +22,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var viewCountLabel: UILabel!
     @IBOutlet weak var videoDescription: UITextView!
     @IBOutlet weak var videoDate: UILabel!
+    @IBOutlet weak var tagLabel: UILabel!
     @IBOutlet weak var likeButton: UIButton!
     
     
@@ -32,6 +39,7 @@ class DetailViewController: UIViewController {
         let snippet = video?.item["snippet"] as! [String : Any]
         let statistics = video?.item["statistics"] as? [String: Any]
         let viewCount = statistics?["viewCount"] as? String
+        let tags = snippet["tags"] as? [String]
         var publishedDate = snippet["publishedAt"] as! String
         let startIndex = publishedDate.index(publishedDate.startIndex, offsetBy: 0)
         let endIndex = publishedDate.index(publishedDate.startIndex, offsetBy: 9)
@@ -48,10 +56,25 @@ class DetailViewController: UIViewController {
         
         titleLabel.text = video?.title
         channelLabel.text = (video?.channelTitle)! + "  ·"
+        var tagText = ""
+        for tag in tags! {
+            tagText += "#\(tag) "
+        }
+        tagLabel.text = tagText
         videoDate.text = "Uploaded at " + publishedDate
         videoDescription.text = snippet["description"] as? String
         likeButton.tintColor = .red
-//        if likedVideo[video?.item["id"] as! String] != nil{
+        
+        // 일반배열
+        if likedVideos.contains(video!) {
+            likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        }
+        else {
+            likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        }
+        
+        // 딕셔너리배열
+//        if likedVideos[video?.item["id"] as! String] != nil{
 //            likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
 //        }
 //        else {
@@ -62,15 +85,34 @@ class DetailViewController: UIViewController {
     }
     
     @objc func ClickButton (_ sender: UIButton) {
-//        if likedVideo[video?.item["id"] as! String] == nil {
-//            likedVideo[video?.item["id"] as! String] = video!
+        // 일반배열
+                if !likedVideos.contains(video!) {
+                    likedVideos.append(video!)
+                    sender.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+                    print("추가:  현재 좋아요 한 비디오갯수는 \(likedVideos.count)")
+                }
+                else {
+                    for i in 0..<likedVideos.count {
+                        if likedVideos[i] == video {
+                            print("\(i+1)번째 비디오 삭제")
+                            likedVideos.remove(at: i)
+                            break
+                        }
+                    }
+                    sender.setImage(UIImage(systemName: "heart"), for: .normal)
+                }
+        
+        // 딕셔너리배열
+//        if likedVideos[video?.item["id"] as! String] == nil {
+//            likedVideos[video?.item["id"] as! String] = video!
 //            sender.setImage(UIImage(systemName: "heart.fill"), for: .normal)
 //        }
 //        else {
-//            likedVideo.removeValue(forKey: video?.item["id"] as! String)
+//            likedVideos.removeValue(forKey: video?.item["id"] as! String)
 //            sender.setImage(UIImage(systemName: "heart"), for: .normal)
+//            print("\(video!.title) 삭제")
 //        }
-//        print(likedVideo.count)
+//        print("좋아요한 비디오 \(likedVideos.count)개")
     }
     
     // WKWebView에 영상의 id를 이용하여 유튜브에서 영상을 로드하는 함수
