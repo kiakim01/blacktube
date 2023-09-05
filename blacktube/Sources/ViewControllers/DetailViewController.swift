ebd493388cc935a205786d0551cb095290a72d59
chanho lee <leech3878@naver.com>
2023-09-05 16:02:20 +0900

    DetailPage UI구성

--- blacktube/Sources/ViewControllers/DetailViewController.swift
+++ blacktube/Sources/ViewControllers/DetailViewController.swift
@@ -6,25 +6,58 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
+    
     var video: Video?
+    @IBOutlet weak var videoView: WKWebView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var channelLabel: UILabel!
+    @IBOutlet weak var viewCountLabel: UILabel!
+    @IBOutlet weak var videoDescription: UITextView!
+    @IBOutlet weak var videoDate: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let videoID = video?.item["id"] as! String
+        SetupUI()
+        LoadVideo(videoID)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 영상의 타이틀, 채널명, 조회수, 설명란을 로드하는 함수
+    func SetupUI () {
+        
+        let snippet = video?.item["snippet"] as! [String : Any]
+        let statistics = video?.item["statistics"] as? [String: Any]
+        let viewCount = statistics?["viewCount"] as? String
+        var publishedDate = snippet["publishedAt"] as! String
+        let startIndex = publishedDate.index(publishedDate.startIndex, offsetBy: 0)
+        let endIndex = publishedDate.index(publishedDate.startIndex, offsetBy: 9)
+        publishedDate = String(publishedDate[startIndex...endIndex])
+        
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        if let viewCountInt = Int(viewCount!){
+            if let formattedViewCount = formatter.string(from: NSNumber(value:viewCountInt)) {
+                viewCountLabel.text = "\(formattedViewCount) views"
+            }
+        }
+        
+        titleLabel.text = video?.title
+        channelLabel.text = (video?.channelTitle)! + "  ·"
+        videoDate.text = "Uploaded at " + publishedDate
+        videoDescription.text = snippet["description"] as? String
+        
     }
-    */
+    
+    // WKWebView에 영상의 id를 이용하여 유튜브에서 영상을 로드하는 함수
+    func LoadVideo (_ id: String) {
+        let myURL = URL(string: "https://www.youtube.com/embed/\(id)")
+        let youtubeRequest = URLRequest(url: myURL!)
+        videoView.load(youtubeRequest)
+    }
 
 }
