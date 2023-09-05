01e278ad182202db438535279d1c742dfdd73894
sanc93 <sanc93@me.com>
2023-09-05 10:07:23 +0900

    ADD: MyPageViewController 생성

---
+++ blacktube/Sources/ViewControllers/MyPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MyPageViewController.swift
+//  blacktube
+//
+//  Created by Sanghun K. on 2023/09/05.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
