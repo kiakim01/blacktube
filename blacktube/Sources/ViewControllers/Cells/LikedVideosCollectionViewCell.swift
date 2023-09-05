256c72c2f99f90d47a82da1d35d24db7c47d15e7
sanc93 <sanc93@me.com>
2023-09-05 11:47:12 +0900

    ADD: 컬렉션 뷰 생성 및 화면 틀 잡기

---
+++ blacktube/Sources/ViewControllers/Cells/LikedVideosCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LikedVideosCollectionViewCell.swift
+//  blacktube
+//
+//  Created by Sanghun K. on 2023/09/05.
+//
+
+import UIKit
+
+class LikedVideosCollectionViewCell: UICollectionViewCell {
+    
+}
--- blacktube/Sources/ViewControllers/MyPageViewController.swift
+++ blacktube/Sources/ViewControllers/MyPageViewController.swift
@@ -11,11 +11,38 @@ class MyPageViewController: UIViewController {
     
     // MARK: - Properties
     
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var userEmailLabel: UILabel!
+    @IBOutlet var logOutButton: UIButton!
+    
+    @IBOutlet var likedVideosCollectionView: UICollectionView!
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        
+        likedVideosCollectionView.delegate = self
+        likedVideosCollectionView.dataSource = self
     }
 
 
+}
+
+// MARK: - Collection View
+extension MyPageViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LikedVideosCollectionViewCell", for: indexPath)
+            return cell
+    }
+    
+    
+}
+
+extension MyPageViewController: UICollectionViewDelegate {
+    
 }
