a273c60afca86b59d56236319eb5642b2f1e593a
sanc93 <sanc93@me.com>
2023-09-08 13:52:32 +0900

    FIX: Collection View 에러 해결, 좋아요 동영상 없을 시 빈 셀 표시하도록 설정

--- blacktube/Sources/ViewControllers/MyPageViewController.swift
+++ blacktube/Sources/ViewControllers/MyPageViewController.swift
@@ -11,6 +11,9 @@ class MyPageViewController: UIViewController {
     
     // MARK: - Properties
 
+
+    @IBOutlet var navigationBar: UINavigationBar!
+    
     @IBOutlet var userImage: UIImageView!
     @IBOutlet var userNameLabel: UILabel!
     @IBOutlet var userEmailLabel: UILabel!
@@ -19,15 +22,16 @@ class MyPageViewController: UIViewController {
     
     @IBOutlet var likedVideosCollectionView: UICollectionView!
     
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        
         NotificationCenter.default.addObserver(self, selector: #selector(refreshUserData), name: NSNotification.Name(rawValue: "refresh"), object: nil)
         
         likedVideosCollectionView.delegate = self
         likedVideosCollectionView.dataSource = self
+        navigationBar.delegate = self
 //        inputDummyToUserDefaults() // 0. UserDefaults에 더미 넣기(테스트용)
         loadDataFromUserDefaults() // 1. UserDefaults에서 불러오기(테스트용)
         configureUI()
@@ -52,6 +56,12 @@ class MyPageViewController: UIViewController {
         }
         userNameLabel.text = loginUser.userName
         userEmailLabel.text = loginUser.userEmail
+        setAppLogoToNavigationBar()
+    }
+    func setAppLogoToNavigationBar() {
+        let logoImageView = UIImageView(image: UIImage(named: "blacktube_applogo_black"))
+        logoImageView.frame = CGRect(x: 18, y: 3, width: 45, height: 31)
+        navigationBar.addSubview(logoImageView)
     }
     
     @objc private func refreshUserData(notification: NSNotification){
@@ -109,27 +119,56 @@ class MyPageViewController: UIViewController {
 // MARK: - Collection View
 extension MyPageViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return likedVideos.count
+        if likedVideos.count == 0 {
+            return 1
+        } else {
+            return likedVideos.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LikedVideosCollectionViewCell", for: indexPath) as! LikedVideosCollectionViewCell
         cell.layer.cornerRadius = 10
         
-        let video = likedVideos[indexPath.item]
-        cell.configure(video)
-        
+        if likedVideos.count == 0 {
+            cell.thumbnailImage.isHidden = true
+            cell.titleLabel.isHidden = true
+            cell.channelLabel.isHidden = true
+            cell.viewCountLabel.isHidden = true
+            cell.plusButton.isHidden = false
+        } else {
+            cell.thumbnailImage.isHidden = false
+            cell.titleLabel.isHidden = false
+            cell.channelLabel.isHidden = false
+            cell.viewCountLabel.isHidden = false
+            cell.plusButton.isHidden = true
+            let video = likedVideos[indexPath.item]
+            cell.configure(video)
+        }
+//        likedVideosCollectionView.reloadData()
         return cell
+        
     }
 }
 
 extension MyPageViewController: UICollectionViewDelegate {
     
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "likedVideoCell", likedVideos.count == 0 {
+            return false // segue를 막음
+        }
+        return true // segue를 실행
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "likedVideoCell", let detailVC = segue.destination as? DetailViewController, let selectedIndexPath = likedVideosCollectionView.indexPathsForSelectedItems?.first {
+        if likedVideos.count != 0, segue.identifier == "likedVideoCell", let detailVC = segue.destination as? DetailViewController, let selectedIndexPath = likedVideosCollectionView.indexPathsForSelectedItems?.first {
             detailVC.video = likedVideos[selectedIndexPath.item]
         }
     }
 }
 
-
+extension MyPageViewController: UINavigationBarDelegate {
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
